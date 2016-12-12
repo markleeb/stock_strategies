54dbff74258ed1a0e8e44e45b4ed18a381269a2a
markleeb <markleebauman@gmail.com>
2016-12-11 20:42:16 -0700

    works for recent year of data
    TODO: argparse
    TODO: address issue with historic stock split and dividend pricing issue

--- get_quotes.py
+++ get_quotes.py
@@ -16,6 +16,9 @@ def historic_prices(ticker, date):
             data_append['Ticker'] = t
             price_data = price_data.append(data_append)
 
+        except KeyboardInterrupt:
+            raise
+
         except: 
             print('cannot find data for stock {ticker} on date {date}'.format(ticker=t, date=date.strftime("%B %d, %Y")))
             continue
@@ -36,6 +39,9 @@ def corporate_actions(ticker, start, end):
             data_append['Ticker'] = t
             action_data = action_data.append(data_append)
 
+        except KeyboardInterrupt:
+            raise
+        
         except:
             print('cannot find data for stock {ticker} from dates {start} to {end}'.format(ticker=t, start=start.strftime("%B %d, %Y"), end=end.strftime("%B %d, %Y")))
             continue
@@ -53,7 +59,7 @@ def corporate_actions(ticker, start, end):
 #    pass
 
 
-def returns(ticker, start, end):
+def returns(price_start, price_end, actions):
     """Takes price difference between two dates, and adjusts for dividends and splits to produce total return as a percent.
 
     :ticker: list of ticker symbols 
@@ -62,42 +68,38 @@ def returns(ticker, start, end):
     :returns: total return as a percentage 
 
     """
-    #TODO: Need to confirm, but it seems as though the historic dividends data represents the dividend assuming post-split price.
-    #So we should only need to multiply the start date closing price by the split value (1/10 split has value '10')
+
+
+    #should only need to multiply the start date closing price by the split value (1/10 split has value '10')
     #1. add all dividends onto end price
     #2. multiply start close price by split
     
-    #create price and actions dataframes
-    price_start = historic_prices(ticker, start)
-    price_end = historic_prices(ticker, end)
-    actions = corporate_actions(ticker, start, end)
+    #TODO:
+    #Yahoo finance assumes most recent split price when reporting historic dividends. 
+    #This will be a problem when adding dividend info during backtesting.  
+    #Might be better to either use div yield or google finance
+
+    
     
     #join price_start with price_end on 'Ticker'
-    prices = pd.merge(price_start, price_end, on='Ticker', suffixes=['_start','_end'])
-    prices = prices[['Close_start', 'Close_end', 'Ticker']]
+    prices = pd.merge(price_start, price_end, on='Ticker', suffixes=['_start','_end'])[['Close_start', 'Close_end', 'Ticker']]
 
-    #get sum of dividends
-    div_totals = actions.groupby('Ticker', as_index=False)['value'].sum()
-    
-    #get stock split info
-    splits = actions[actions.action=='SPLIT']
-    
-    #join prices with div_totals to get div col
-    prices_div = pd.merge(prices, div_totals, on='Ticker', how='left')
-    prices_div.rename(columns = {'value':'dividends'}, inplace=True)
-    prices_div['dividends'] = prices_div['dividends'].fillna(0)
+    #get sum of dividends and product of splits
+    div_totals = actions[actions.action=='DIVIDEND'].groupby('Ticker', as_index=False)['value'].sum().rename(columns = {'value':'dividends'})
+    split_totals = actions[actions.action=='SPLIT'].groupby('Ticker', as_index=False)['value'].prod().rename(columns = {'value':'splits'})
 
-    prices_div['div_adj_Close_end'] = prices_div.Close_end+prices_div.dividends
-    #join prices with splits to get split col
-    #totals = prices_div.merge(splits, on='Ticker', how='left')
-   
-    prices_div_split = pd.merge(prices_div, splits, on='Ticker', how='left')
-    prices_div_split.rename(columns = {'value':'split'}, inplace=True)
-    prices_div_split['split'] = prices_div['split'].fillna(1)
+    #join prices with div_totals and split_totals to get div and split cols
+    prices_div_split = pd.merge(pd.merge(prices, div_totals, on='Ticker', how='left'), split_totals, on='Ticker', how='left')
 
-    prices_div_split['split_adj_Close_start'] = prices_div_split.Close_start*prices_div_split.split
+    #fill NaNs with 0 for dividends, and 1 for splits (sum dividends, multiply splits)
+    prices_div_split['dividends'] = prices_div_split['dividends'].fillna(0)
+    prices_div_split['splits'] = prices_div_split['splits'].fillna(1)
 
-    prices_div_split['perc_change'] = (prices_div_split.div_adj_Close_end - prices_div_split.split_adj_Close_start)/prices_div.split_adj_Close_start
+    #adjust start and end close prices based on dividend and split info
+    prices_div_split['div_adj_Close_end'] = (prices_div_split.Close_end + prices_div_split.dividends)
+    prices_div_split['split_adj_Close_start'] = (prices_div_split.Close_start * prices_div_split.splits)
+
+    prices_div_split['perc_change'] = (prices_div_split.div_adj_Close_end - prices_div_split.split_adj_Close_start)/prices_div_split.split_adj_Close_start
 
     change = prices_div_split.perc_change.mean()
     
@@ -108,3 +110,13 @@ def main():
     """program logic"""
 
     #convert dates to datetime
+
+    #create price and actions dataframes
+    price_start = historic_prices(ticker, start)
+    price_end = historic_prices(ticker, end)
+    actions = corporate_actions(ticker, start, end)
+
+    prices_div_split, change = returns(price_start, price_end, actions)
+
+if __name__=='__main__':
+    main
